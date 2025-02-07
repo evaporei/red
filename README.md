05cd3e305de688e973d19f56c93e9e45ca08a50e
Eva Pace <eba.pachi@gmail.com>
2025-02-07 12:05:58 -0300

    render red window

---
+++ README.md
@@ -0,0 +1,10 @@
+# red
+
+Rust text EDitor.
+
+To run on MacOS:
+
+```bash
+$ export LIBRARY_PATH="$LIBRARY_PATH:$(brew --prefix)/lib"
+$ cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use sdl2::event::Event;
+use sdl2::pixels::Color;
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem
+        .window("red", 800, 600)
+        .position_centered()
+        .resizable()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window
+        .into_canvas()
+        .accelerated()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut event_pump = sdl_context.event_pump()?;
+
+    let mut quit = false;
+    while !quit {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => quit = true,
+                _ => {}
+            }
+        }
+
+        canvas.set_draw_color(Color::RED);
+        canvas.clear();
+        canvas.present();
+    }
+
+    Ok(())
 }
