967f89371480775a6b30d7fa5749b4b645035971
Eva Pace <eba.pachi@gmail.com>
2025-02-08 12:35:46 -0300

    render font spritesheet

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,49 @@
 use sdl2::event::Event;
 use sdl2::pixels::Color;
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::rect::Rect;
+use sdl2::surface::Surface;
+use stb_image::stb_image::stbi_load;
+use std::ffi::CString;
+
+fn load_img(file_path: &str) -> (Vec<u8>, i32, i32) {
+    let c_path = CString::new(file_path).unwrap();
+
+    let mut width = 0;
+    let mut height = 0;
+    let mut channels = 0;
+    let stbi_rgb_alpha = 4;
+
+    let pixels = unsafe {
+        stbi_load(
+            c_path.as_ptr(),
+            &mut width,
+            &mut height,
+            &mut channels,
+            stbi_rgb_alpha,
+        )
+    };
+
+    if pixels.is_null() {
+        panic!("image is null after load");
+    }
+
+    (
+        unsafe { std::slice::from_raw_parts(pixels, (width * height * 4) as usize) }.to_vec(),
+        width,
+        height,
+    )
+}
+
+fn surface_from_file(pixels: &mut Vec<u8>, width: i32, height: i32) -> Result<Surface, String> {
+    Surface::from_data(
+        pixels,
+        width as u32,
+        height as u32,
+        (4 * width) as u32,
+        PixelFormatEnum::RGBA32,
+    )
+}
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
@@ -18,6 +62,14 @@ fn main() -> Result<(), String> {
         .build()
         .map_err(|e| e.to_string())?;
 
+    let (mut pixels, width, height) = load_img("charmap-oldschool_white.png");
+    let font_surface = surface_from_file(&mut pixels, width, height)?;
+    let texture_creator = canvas.texture_creator();
+    let font_texture = font_surface
+        .as_texture(&texture_creator)
+        .map_err(|e| e.to_string())?;
+    let font_rect = Rect::new(0, 0, font_surface.width(), font_surface.height());
+
     let mut event_pump = sdl_context.event_pump()?;
 
     let mut quit = false;
@@ -29,8 +81,10 @@ fn main() -> Result<(), String> {
             }
         }
 
-        canvas.set_draw_color(Color::RED);
+        canvas.set_draw_color(Color::BLACK);
         canvas.clear();
+
+        canvas.copy(&font_texture, font_rect, font_rect)?;
         canvas.present();
     }
 
